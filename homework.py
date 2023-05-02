9cfccaded3ccdd8d777c43ca2a82ec156f620004
Вячеслав Прасолов <Prasolov.vyacheslav@yandex.ru>
2023-05-02 22:33:46 +0300

    Исправление main

--- homework.py
+++ homework.py
@@ -46,12 +46,22 @@ def get_api_answer(current_timestamp: int) -> Union[dict, str]:
         url=ENDPOINT,
         headers=HEADERS,
         params={'from_date': current_timestamp})
+    logging.info(
+        f'Отправка запроса на {parameters["url"]}'
+        f'с параметрами {parameters["params"]}')
     try:
         response = requests.get(**parameters)
-    except requests.exceptions.RequestException as exc:
-        raise ConnectionError(f'Ошибка подключения: {exc}')
+    except requests.exceptions.RequestException:
+        raise ConnectionError('Ошибка подключения:'
+                              'Невозможно подключиться к {},'
+                              'параметры: {}'.format(parameters['url'],
+                                                     parameters['params']))
     if response.status_code != HTTPStatus.OK:
-        raise HTTPRequestError('Код ответа от API не 200.')
+        error_code = response.status_code
+        error_reas = response.reason
+        error_text = response.text
+        error_message = f'Ошибка HTTP {error_code}: {error_reas}\n{error_text}'
+        raise HTTPRequestError(error_message)
     return response.json()
 
 
@@ -71,16 +81,16 @@ def check_response(response: dict):
 
 
 def parse_status(homework: dict) -> str:
-    """Извлекает из информации и статус конкретной домашней работы."""
+    """Извлекает из информации статус конкретной домашней работы."""
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
 
     if not homework_name:
         raise KeyError('Не нашли homework_name в homework')
     if homework_status not in HOMEWORK_VERDICTS:
-        raise KeyError(
-            f'Недокументированный статус {homework_name}'
-            ' домашней работы в ответе API'
+        raise ValueError(
+            f'Неожиданное значение статуса "{homework_status}"'
+            f'для работы "{homework_name}"'
         )
 
     verdict = HOMEWORK_VERDICTS[homework_status]
@@ -100,46 +110,50 @@ def check_tokens() -> bool:
 
 def main():
     """Основная логика работы бота."""
-    prev_report = {}
-    current_report = {}
-    last_send = {
-        'error': None,
-    }
     if not check_tokens():
         logging.critical(
             'Отсутствует обязательная переменная окружения.\n'
             'Программа принудительно остановлена.'
         )
         raise KeyError('Отсутствует обязательная переменная окружения.')
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = 0
+    prev_report = {}
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
-            for homework in homeworks:
-                if homeworks:
-                    current_report = {
-                        'homework_name': [homework['homework_name']],
-                        'status': parse_status(homework)}
-                    if current_report != prev_report:
-                        message = parse_status(homework)
-                        if send_message(bot, message):
-                            send_message(bot, message)
-                            continue
-                        else:
-                            break
-            current_timestamp = response.get('current_date')
-            prev_report = current_report.copy()
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            if homeworks:
+                current_report = {
+                    'homework_name': homeworks[0]['homework_name'],
+                    'status': homeworks[0]['status']
+                }
+                message = parse_status(current_report)
+            else:
+                message = 'Нет новых статусов'
+                current_report = {
+                    'homework_name': None,
+                    'status': message
+                }
+            if current_report != prev_report:
+                if send_message(bot, message):
+                    prev_report = current_report.copy()
+                    current_timestamp = response.get('current_date')
+                else:
+                    logging.info('Не удалось отправить сообщение')
+            else:
+                logging.info('Статусы не изменились')
+        except EmptyResponseFromAPI as error:
             logging.error(error)
-            if last_send['error'] != message:
-                send_message(bot, message)
-                last_send['error'] = message
-        else:
-            last_send['error'] = None
+        except Exception as error:
+            current_report = {
+                'homework_name': None,
+                'status': f'Сбой в работе программы: {error}'
+            }
+            logging.error(error)
+            if current_report != prev_report:
+                send_message(bot, current_report['status'])
+                prev_report = current_report.copy()
         finally:
             time.sleep(RETRY_PERIOD)
 
