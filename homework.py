7f6e6eeaba729bda82dc90e1bad4a246d0f6b5d7
Вячеслав Прасолов <Prasolov.vyacheslav@yandex.ru>
2023-05-03 13:45:24 +0300

    Исправление ряда ошибок

--- homework.py
+++ homework.py
@@ -47,20 +47,25 @@ def get_api_answer(current_timestamp: int) -> Union[dict, str]:
         headers=HEADERS,
         params={'from_date': current_timestamp})
     logging.info(
-        f'Отправка запроса на {parameters["url"]}'
-        f'с параметрами {parameters["params"]}')
+        f'Отправка запроса на {ENDPOINT} '
+        f'с параметрами {parameters["params"]} '
+        f'и загаловками {HEADERS}'
+    )
     try:
         response = requests.get(**parameters)
     except requests.exceptions.RequestException:
-        raise ConnectionError('Ошибка подключения:'
-                              'Невозможно подключиться к {},'
-                              'параметры: {}'.format(parameters['url'],
-                                                     parameters['params']))
+        raise ConnectionError(
+            f'Ошибка подключения: '
+            f'Невозможно подключиться к {ENDPOINT} '
+            f'параметры: {parameters["params"]}, '
+            f'заголовки: {HEADERS}'
+        )
     if response.status_code != HTTPStatus.OK:
-        error_code = response.status_code
-        error_reas = response.reason
-        error_text = response.text
-        error_message = f'Ошибка HTTP {error_code}: {error_reas}\n{error_text}'
+        error_message = (
+            f'Ошибка HTTP {response.status_code}: {response.reason}\n'
+            f'{response.text}'
+        )
+
         raise HTTPRequestError(error_message)
     return response.json()
 
@@ -119,38 +124,34 @@ def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = 0
     prev_report = {}
+    current_report = {
+        'homework_name': None,
+        'status': None
+    }
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
             if homeworks:
-                current_report = {
-                    'homework_name': homeworks[0]['homework_name'],
-                    'status': homeworks[0]['status']
-                }
+                current_report['homework_name'] = homeworks[0]['homework_name']
+                current_report['status'] = homeworks[0]['status']
                 message = parse_status(current_report)
+                current_report['status'] = message
             else:
                 message = 'Нет новых статусов'
-                current_report = {
-                    'homework_name': None,
-                    'status': message
-                }
+                current_report['status'] = message
             if current_report != prev_report:
                 if send_message(bot, message):
                     prev_report = current_report.copy()
-                    current_timestamp = response.get('current_date')
-                else:
-                    logging.info('Не удалось отправить сообщение')
+                    current_timestamp = response.get('current_date', 0)
             else:
                 logging.info('Статусы не изменились')
         except EmptyResponseFromAPI as error:
             logging.error(error)
         except Exception as error:
-            current_report = {
-                'homework_name': None,
-                'status': f'Сбой в работе программы: {error}'
-            }
-            logging.error(error)
+            current_report['status'] = f'Сбой в работе программы: {error}'
+            logging.error(f'Произошла ошибка: {current_report.get("status")},'
+                          f'{error}')
             if current_report != prev_report:
                 send_message(bot, current_report['status'])
                 prev_report = current_report.copy()
